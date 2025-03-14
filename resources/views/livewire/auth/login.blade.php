b66b07fd3a9cc4e016049b05e61c91faa5b78ae4
mjmiri68 <ceo.narvan@gmail.com>
2025-03-14 17:25:39 +0330

    change UI Login / Register

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,24 +1,57 @@
-<div>
-    <form wire:submit.prevent="login">
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" wire:model="email">
+<form class="flex flex-col gap-6" novalidate="novalidate"  wire:submit.prevent="login">
+    <input id="plan" type="hidden" value="" name="plan">
+
+    <div class="relative">
+        <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200 mb-3" for="email">
+            <span class="">Email Address</span>
+        </label>
+        <input id="email" class="block peer w-full px-4 py-3 border border-gray-300 bg-gray-100 text-gray-800 placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 placeholder-gray-400 transition-colors focus:border-indigo-500 focus:outline-0 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" type="email" wire:model="email" placeholder="you@example.com">
+    </div>
+
+    <div class="relative">
+        <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200 mb-3" for="password">
+            <span class="">Password</span>
+        </label>
+        <div class="relative">
+            <input id="password" class="block peer w-full px-4 py-3 border border-gray-300 bg-gray-100 text-gray-800 placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 placeholder-gray-400 transition-colors focus:border-indigo-500 focus:outline-0 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" type="password" wire:model="password" placeholder="Your password">
+            <button class="lqd-show-password absolute right-3 top-1/2 z-10 inline-flex -translate-y-1/2 cursor-pointer items-center justify-center rounded transition-colors hover:bg-gray-200 dark:hover:bg-gray-600" type="button" @click="toggleType()">
+                <svg stroke-width="1.5" :class="inputValueVisible ? 'hidden' : ''" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+                </svg>
+                <svg stroke-width="1.5" :class="inputValueVisible ? 'block' : 'hidden'" class="hidden w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828"></path>
+                    <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87"></path>
+                    <path d="M3 3l18 18"></path>
+                </svg>
+            </button>
         </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" wire:model="password">
+    </div>
+
+    <div class="my-2 flex justify-between gap-2">
+        <div class="grow">
+            <div class="relative">
+                <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200" for="remember">
+                    <input id="remember" class="peer rounded border-gray-300 dark:border-gray-600 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" name="remember" type="checkbox">
+                    <span class="">Remember me</span>
+                </label>
+            </div>
         </div>
-        <button type="submit">Login</button>
-    </form>
-    @if (session()->has('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
+        <div class="text-right">
+            <a class="text-indigo-600 dark:text-indigo-400" href="/forgot-password">Forgot Password?</a>
         </div>
-    @elseif (session()->has('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
-    <a href="{{ route('register') }}">Register</a>
-    <button class="btn btn-primary">بوت استرپ کار می‌کند!</button>
-</div>
+    </div>
+
+    <input class="hidden" id="recaptcha" value="0">
+    <button class="lqd-btn group inline-flex items-center justify-center gap-1.5 font-medium rounded-full transition-all hover:-translate-y-0.5 hover:shadow-xl lqd-btn-primary bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:bg-indigo-700 focus-visible:shadow-indigo-300/10 px-5 py-3" id="LoginFormButton" type="submit">
+        Sign in
+    </button>
+    <div class="text-gray-600 dark:text-gray-400">
+        By proceeding, you acknowledge and accept our
+        <a class="font-medium text-indigo-600 underline" href="/terms" target="_blank">Terms and
+            Conditions</a>
+        and
+        <a class="font-medium text-indigo-600 underline" href="/privacy-policy" target="_blank">Privacy
+            Policy</a>.
+    </div>
+</form>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,34 +1,77 @@
-<div>
-    <form wire:submit='submit'>
-        <div class="mb-4">
-            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name</label>
-            <input type="text" wire:model='name' class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            @error('name') <span class="text-red-500">{{ $message }}</span> @enderror
-        </div>
-        <div class="mb-4">
-            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-            <input type="email" wire:model='email' class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
-        </div>
-        <div class="mb-4">
-            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input type="password" wire:model='password' class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+<form class="flex flex-col gap-6" novalidate="novalidate"  wire:submit='submit'>
+    <input id="plan" type="hidden" value="" name="plan">
+    <div class="relative">
+        <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200 mb-3" for="email">
+            <span class="">Your name</span>
+        </label>
+        <input id="name" class="block peer w-full px-4 py-3 border border-gray-300 bg-gray-100 text-gray-800 placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 placeholder-gray-400 transition-colors focus:border-indigo-500 focus:outline-0 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" type="text" wire:model="name" placeholder="John">
+        @error('name') <span class="text-red-500">{{ $message }}</span> @enderror
+    </div>
+    <div class="relative">
+        <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200 mb-3" for="email">
+            <span class="">Email Address</span>
+        </label>
+        <input id="email" class="block peer w-full px-4 py-3 border border-gray-300 bg-gray-100 text-gray-800 placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 placeholder-gray-400 transition-colors focus:border-indigo-500 focus:outline-0 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" type="email" wire:model="email" placeholder="you@example.com">
+        @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
+    </div>
+
+    <div class="relative">
+        <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200 mb-3" for="password">
+            <span class="">Password</span>
+        </label>
+        <div class="relative">
+            <input id="password" class="block peer w-full px-4 py-3 border border-gray-300 bg-gray-100 text-gray-800 placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 placeholder-gray-400 transition-colors focus:border-indigo-500 focus:outline-0 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" type="password" wire:model="password" placeholder="Your password">
+            <button class="lqd-show-password absolute right-3 top-1/2 z-10 inline-flex -translate-y-1/2 cursor-pointer items-center justify-center rounded transition-colors hover:bg-gray-200 dark:hover:bg-gray-600" type="button" @click="toggleType()">
+                <svg stroke-width="1.5" :class="inputValueVisible ? 'hidden' : ''" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+                </svg>
+                <svg stroke-width="1.5" :class="inputValueVisible ? 'block' : 'hidden'" class="hidden w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828"></path>
+                    <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87"></path>
+                    <path d="M3 3l18 18"></path>
+                </svg>
+            </button>
             @error('password') <span class="text-red-500">{{ $message }}</span> @enderror
         </div>
-        <div class="mb-4">
-            <label for="password_confirmation" class="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
-            <input type="password" wire:model='password_confirmation' class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="relative">
+        <label class="flex cursor-pointer items-center gap-2 text-xs font-medium leading-none text-gray-700 dark:text-gray-200 mb-3" for="password">
+            <span class="">Confirm Password</span>
+        </label>
+        <div class="relative">
+            <input id="password" class="block peer w-full px-4 py-3 border border-gray-300 bg-gray-100 text-gray-800 placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 placeholder-gray-400 transition-colors focus:border-indigo-500 focus:outline-0 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500" type="password" wire:model="password_confirmation" placeholder="repeat your password">
+            <button class="lqd-show-password absolute right-3 top-1/2 z-10 inline-flex -translate-y-1/2 cursor-pointer items-center justify-center rounded transition-colors hover:bg-gray-200 dark:hover:bg-gray-600" type="button" @click="toggleType()">
+                <svg stroke-width="1.5" :class="inputValueVisible ? 'hidden' : ''" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+                </svg>
+                <svg stroke-width="1.5" :class="inputValueVisible ? 'block' : 'hidden'" class="hidden w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828"></path>
+                    <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87"></path>
+                    <path d="M3 3l18 18"></path>
+                </svg>
+            </button>
             @error('password_confirmation') <span class="text-red-500">{{ $message }}</span> @enderror
         </div>
-        <div class="flex items
-        -center justify-between">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Register
-            </button>
-            <a href="{{ route('login') }}" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-                Already have an account?
-            </a>
-        </div>
+    </div>
 
-    </form>
-</div>
+    <div class="my-2 flex justify-between gap-2">
+        <div class="text-right">
+            <a class="text-indigo-600 dark:text-indigo-400" href="/forgot-password">Forgot Password?</a>
+        </div>
+    </div>
+
+    <input class="hidden" id="recaptcha" value="0">
+    <button class="lqd-btn group inline-flex items-center justify-center gap-1.5 font-medium rounded-full transition-all hover:-translate-y-0.5 hover:shadow-xl lqd-btn-primary bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:bg-indigo-700 focus-visible:shadow-indigo-300/10 px-5 py-3" id="LoginFormButton" type="submit">
+        Sign Up
+    </button>
+    <div class="text-gray-600 dark:text-gray-400">
+        By proceeding, you acknowledge and accept our
+        <a class="font-medium text-indigo-600 underline" href="/terms" target="_blank">Terms and
+            Conditions</a>
+        and
+        <a class="font-medium text-indigo-600 underline" href="/privacy-policy" target="_blank">Privacy
+            Policy</a>.
+    </div>
+</form>
